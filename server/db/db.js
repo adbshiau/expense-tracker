89eaf041fe2a01837f3f6b3b0d160b3166e13854
AyaShiau <63468278+adbshiau@users.noreply.github.com>
2023-08-01 13:22:55 -0700

    GlobalStyle component created

--- server/db/db.js
+++ server/db/db.js
@@ -4,7 +4,7 @@ const db = async () => {
   try {
     mongoose.set("strictQuery", false);
     await mongoose.connect(process.env.MONGO_URL);
-    console.log("DB Connected");
+    console.log("DB connected");
   } catch (error) {
     console.log("DB Connection Error");
   }
